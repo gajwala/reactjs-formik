51948b44d7aeb4b1e04d5ae828f2f9dbb39bcebe
narayana bathi <narayanabathi@narayanas-MacBook-Pro.local>
2020-10-01 16:22:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './App.css'
+import FormikContainer from './components/FormikContainer'
+import LoginForm from './components/LoginForm'
+import RegistrationForm from './components/RegistrationForm'
+import EnrollmentForm from './components/EnrollmentForm'
+import { theme, ThemeProvider } from '@chakra-ui/core'
+
+function App() {
+  return (
+
+    <ThemeProvider theme={theme}>
+    <div className='App'>
+      {/* <FormikContainer /> */}
+      <LoginForm />
+      {/* <RegistrationForm /> */}
+      {/* <EnrollmentForm /> */}
+    </div>
+  </ThemeProvider>
+    // <div className="App">
+    //  {/* <YoutubeForm/> */}
+
+    //  {/* <FormikContainer/> */}
+    // </div>
+  );
+}
+
+export default App;
